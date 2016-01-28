c286ccbed5723fa268cdd59220bd7022c2bb7197
Euan Walls <apps@inocr8.co>
2016-01-28 11:50:28 +0000

    test created for dice to confirm it has sides

--- snakes_and_ladders_spec.js
+++ snakes_and_ladders_spec.js
@@ -2,6 +2,7 @@ var assert = require('assert');
 
 var Player = require('./player');
 var Game = require('./game');
+var Dice = require('.dice');
 
 describe('Player', function(){
   it('should have a name', function(){
@@ -18,5 +19,12 @@ describe('Game', function(){
   it('should have a name', function(){
     var game = new Game('First Game');
       assert.equal('First Game', game.name);
+  });
+});
+
+describe('Dice', function(){
+  it('should have sides', function(){
+    var dice = new Dice(6);
+    assert.equal(6, dice.sides);
   });
 });
