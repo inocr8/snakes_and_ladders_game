d8a290827f3369af8e21be8fb53bf5da6f82d831
Euan Walls <apps@inocr8.co>
2016-01-28 11:41:57 +0000

    game test added

--- snakes_and_ladders_spec.js
+++ snakes_and_ladders_spec.js
@@ -1,10 +1,18 @@
 var assert = require('assert');
 
 var Player = require('./player');
+var Game = require('./game');
 
 describe('Player', function(){
   it('should have a name', function(){
     var player = new Player('Euan');
     assert.equal('Euan', player.name); 
+  });
+});
+
+describe('Game', function(){
+  it('should have a name', function(){
+    var game = new Game('First Game');
+      assert.equal('First Game', game.name);
   });
 });
