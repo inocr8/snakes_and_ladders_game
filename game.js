9665c8e2899317879472b8718a65dc816dc45961
Euan Walls <apps@inocr8.co>
2016-01-28 13:37:15 +0000

    function added for game to set current player and check current player

--- game.js
+++ game.js
@@ -1,12 +1,16 @@
 var Game = function(name, dice){
   this.name = name;
   this.players = [];
+  this.currentPlayerIndex = 0;
   this.dice = dice;
 };
 
 Game.prototype = {
   addPlayer: function(player){
     this.players.push(player);
+  },
+  currentPlayer: function(){
+    return this.players[this.currentPlayerIndex];
   },
   movePlayer: function(player, position){
     player.position = position;
