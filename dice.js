b89d3233650bc7f35dbdb4fd2a2e16949983a8a6
Sky Su <februaryskys@gmail.com>
2016-01-28 15:35:53 +0000

    Refactors moves roll to Dice.prototype

--- dice.js
+++ dice.js
@@ -1,8 +1,11 @@
 var Dice = function(sides){
   this.sides = sides;
-  this.roll = function(){
+};
+
+Dice.prototype = {
+  roll: function(){
       return Math.floor(Math.random() * (this.sides - 1 + 1)) + 1;
-  };
+  }
 };
 
 module.exports = Dice;
